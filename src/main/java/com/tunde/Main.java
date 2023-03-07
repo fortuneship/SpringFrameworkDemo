aea5e91ab8b5bdd027c390b9698b6bf8716fe571
Tunde Babatunde <tunde8983@gmail.com>
2023-03-07 09:28:00 +0100

    java base configuration

--- src/main/java/com/tunde/Main.java
+++ src/main/java/com/tunde/Main.java
@@ -2,12 +2,15 @@ package com.tunde;
 
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        /*ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");*/
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 
         Doctor staff = context.getBean(Doctor.class);
         staff.assist();
